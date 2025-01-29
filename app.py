00d3f149015f8243a5d4ce9b307a09ab17c306b0
Omkar Sawant Bhosle <omkarsawantbhosle@Omkars-MacBook-Pro.local>
2025-01-28 21:14:31 -0800

    OAuth GAPI and others

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from flask_oauthlib.client import OAuth
 import os
 from openai import OpenAI
+import json
+import websocket
+
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'your_secret_key')
@@ -118,11 +121,38 @@ def query():
     try:
         oaiclient = OpenAI()
         oaiclient.api_key = os.getenv("OPENAI_API_KEY")
+        # example requires websocket-client library:
+        # pip install websocket-client
+
+
+        OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+        url = "wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-12-17"
+        headers = [
+            "Authorization: Bearer " + OPENAI_API_KEY,
+            "OpenAI-Beta: realtime=v1"
+        ]
+        message=[{"role": "system", "content": "You are a helpful assistant"},{"role": "user", "content": user_input}]
+
+        def on_open(ws):
+            print("Connected to server.")
+
+        def on_message(ws, message):
+            data = json.loads(message)
+            print("Received event:", json.dumps(data, indent=2))
+
+        ws = websocket.WebSocketApp(
+            url,
+            header=headers,
+            on_open=on_open,
+            on_message=on_message,
+        )
+
+        ws.run_forever()
 
         response = oaiclient.chat.completions.create(
             model=model,
-            messages=[{"role": "system", "content": "You are a helpful assistant"},
-                      {"role": "user", "content": user_input}],
+            messages=message,
             stream=False
         )
 
